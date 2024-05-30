181feb6f584276ec745b3a96f2b3731c9df91a9f
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-05-30 18:49:23 +0000

    added hours, minutes, + seconds on html, edited js

--- script.js
+++ script.js
@@ -7,20 +7,16 @@
 // when button is clicked, runs timer - basic IDEA of code, still setting up
 function timerStart() {
   // variables
-  let timer = parseInt(document.getElementById("timer-input").value)
+  let timerSeconds = parseInt(document.getElementById("timer-seconds").value)
+  let timerMinutes = parseInt(document.getElementById("timer-minutes").value)
+  let timerHours = parseInt(document.getElementById("timer-hours").value)
   const now = new Date()
   const seconds = now.getSeconds()
-  let counter = timer
 
   while (True)
     // breaks out of loop if timer reaches zero
-    if (timer == 0) {
-      // will add sound file
-      break
-    }
-    
-    // if countdown reaches 0
-    if (hours == 0 && minutes == 0 && seconds == 0) {
+    if (timerHours == 0 && timerMinutes == 0 && timerSeconds == 0) {
       // play noise
+      break
     }
 }
