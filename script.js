6b9cc7b95489e7a65fddc312e6c3f6891a6d841b
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-06-10 18:44:14 +0000

    fixed.. (semi-finished) timer js

--- script.js
+++ script.js
@@ -10,35 +10,25 @@ function timerStart() {
   const timeInHours = parseInt(document.getElementById("timer-hours").value)
   const timeInMinutes = parseInt(document.getElementById("timer-minutes").value)
   const timeInSeconds = parseInt(document.getElementById("timer-seconds").value)
-  let timer = ((timeInHours) * 60 * 60) + ((timeInMinutes) * 60) + timeInSeconds
-  let counter = 0
   let hours = timeInHours
   let minutes = timeInMinutes
   let seconds = timeInSeconds
-
-
-  /* const now = new Date()
-  const seconds = now.getSeconds()
-    let counter = timer */
-
-  const countdownForSeconds = setInterval(function () {
+  
+  const countdown = setInterval(function () {
     // timer goes down by 1
-    counter ++
     seconds += -1
-    if (seconds == 0) {
+    if (hours == 0 && minutes == 0 && seconds == 0) {
+      // play noise
+      document.getElementById("timer-countdown").innerHTML = "DONE"
+      clearInterval(countdown)
+    } else if (seconds == 0 && minutes !== 0) {
       minutes += -1
       seconds = 59
-      if (minutes == 0)
-        hours += -1
-        minutes = 59
+    } else if (minutes == 0 && hours !== 0) {
+      hours += -1
+      minutes = 59
     }
 
     document.getElementById("timer-countdown").innerHTML = hours + "h " + minutes + "m " + seconds + "s"
-
-    if (counter == timer) {
-      // play noise
-      clearInterval(countdownForSeconds)
-      document.getElementById("timer-countdown").innerHTML = "DONE"
-    }
   }, 1000)
 }
